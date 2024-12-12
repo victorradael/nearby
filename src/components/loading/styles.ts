deba53b47f47bcd5c8c75935b7f56e8ece37fb17
Victor Radael <victorradael+github@gmail.com>
2024-12-12 19:51:50 -0300

    feat: :sparkles: Component Updated: loading

--- src/components/loading/styles.ts
+++ src/components/loading/styles.ts
@@ -1,10 +1,15 @@
 import { StyleSheet } from "react-native";
 import { colors } from "@/styles/colors";
+import { fontFamily } from "@/styles/font-family";
 
 export const s = StyleSheet.create({
     container: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center"
+       gap:24,
+       flex:1
+    },
+    title: {
+        fontSize: 16,
+        fontFamily: fontFamily.regular,
+        color: colors.gray[500]
     }
 })
